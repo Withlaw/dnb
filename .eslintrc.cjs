9d4902ada533234c18941011c536d2bcfd0a8e70
Withlaw <car2559@naver.com>
2023-12-30 21:07:16 +0900

    린터, 프리티어 설정 수정

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,17 +1,23 @@
 module.exports = {
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: ["./tsconfig.json", "./tsconfig.node.json"],
+    tsconfigRootDir: __dirname,
+  },
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    // 'react-app',
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-    // "plugin:import/recommended",
-    'prettier',
+    // "react-app",
+    "eslint:recommended",
+    // "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-type-checked",
+    "plugin:react-hooks/recommended",
+    "prettier",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs', '*.config.*'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh', 'import'],
+  ignorePatterns: ["dist", ".eslintrc.cjs", "*.config.*"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh", "import"],
   rules: {
     // // 상대 경로 사용 제한
     // "no-restricted-imports": [
@@ -20,21 +26,21 @@ module.exports = {
     //     patterns: [".*"],
     //   },
     // ],
-    'react-refresh/only-export-components': [
-      'warn',
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
     // 라이브러리 패키지 구문을 제외하고는 확장자 사용
-    'import/extensions': ['error', 'ignorePackages'],
+    "import/extensions": ["error", "ignorePackages"],
   },
   settings: {
     // "import/parsers": {
     //   "@typescript-eslint/parser": [".ts", ".tsx"],
     // },
-    'import/resolver': {
+    "import/resolver": {
       typescript: {
         // "alwaysTryTypes": true,
-        project: './tsconfig.json',
+        project: "./tsconfig.json",
       },
     },
   },
