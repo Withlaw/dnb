ceaa18038084d5d1c76a7b76a0fd1161387669fd
Withlaw <car2559@naver.com>
2023-12-30 20:56:02 +0900

    라이브러리 설치 및 테일윈드 설정

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,15 +3,15 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     // 'react-app',
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react-hooks/recommended",
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
     // "plugin:import/recommended",
-    "prettier",
+    'prettier',
   ],
-  ignorePatterns: ["dist", ".eslintrc.cjs", "node_modules"],
-  parser: "@typescript-eslint/parser",
-  plugins: ["react-refresh", "import"],
+  ignorePatterns: ['dist', '.eslintrc.cjs', '*.config.*'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh', 'import'],
   rules: {
     // // 상대 경로 사용 제한
     // "no-restricted-imports": [
@@ -20,21 +20,21 @@ module.exports = {
     //     patterns: [".*"],
     //   },
     // ],
-    "react-refresh/only-export-components": [
-      "warn",
+    'react-refresh/only-export-components': [
+      'warn',
       { allowConstantExport: true },
     ],
     // 라이브러리 패키지 구문을 제외하고는 확장자 사용
-    "import/extensions": ["error", "ignorePackages"],
+    'import/extensions': ['error', 'ignorePackages'],
   },
   settings: {
     // "import/parsers": {
     //   "@typescript-eslint/parser": [".ts", ".tsx"],
     // },
-    "import/resolver": {
+    'import/resolver': {
       typescript: {
         // "alwaysTryTypes": true,
-        project: "./tsconfig.json",
+        project: './tsconfig.json',
       },
     },
   },
