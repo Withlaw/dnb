e3b0ac6d25764464f4711ea231eaa0070fd6e3ee
Withlaw <car2559@naver.com>
2024-01-01 00:25:05 +0900

    style: root layout 전체적인 스타일 지정

--- src/ui/layout-root.tsx
+++ src/ui/layout-root.tsx
@@ -2,15 +2,21 @@ import { Outlet } from 'react-router-dom';
 
 const RootLayout = () => {
 	return (
-		<div className='container'>
-			<header>app header</header>
-			<aside>app aside</aside>
-			<main>
+		<div className="container mx-auto bg-green-100 h-dvh flex flex-col">
+			<header className="bg-green-600 border-b border-grey-100 border-solid px-6 py-2 w-full h-[50px] flex-initial flex justify-between items-center">
+				<div className="flex">동네북</div>
+				<div className="flex">
+					<p>검색</p>
+					<p>기타</p>
+				</div>
+			</header>
+			{/* <aside className="bg-green-800">app aside</aside> */}
+			<main className="bg-green-400 px-4 pb-[40px] flex-auto">
 				<p>app main</p>
 				<Outlet />
 			</main>
-			<nav>app nav</nav>
-			<footer>app footer</footer>
+			{/* <footer>app footer</footer> */}
+			<nav className="px-4 bg-green-800 h-[50px] flex-initial">app nav</nav>
 		</div>
 	);
 };
