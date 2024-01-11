6494e41285dae87b740ac537320e5f4cc0be1539
Withlaw <car2559@naver.com>
2024-01-11 14:21:30 +0900

    유저 페이지 ui 스케치

--- src/pages/user.tsx
+++ src/pages/user.tsx
@@ -1,3 +1,4 @@
+import { HiMenu } from 'react-icons/hi';
 import { ScrollRestoration, useNavigate } from 'react-router-dom';
 
 import HeaderMain from '@/ui/header-main.tsx';
@@ -17,12 +18,126 @@ const UserPage = () => {
 					<NavHeader />
 				</div>
 				<div>
-					<span className="text-sm tracking-wider">내 정보</span>
+					<span className="text-2xl hover:cursor-pointer">
+						<HiMenu size="24" />
+					</span>
 				</div>
 			</HeaderMain>
 
 			{/* <aside className="bg-green-800">app aside</aside> */}
-			<main className="flex-auto bg-stone-50 px-4 pt-[58px]"></main>
+			<main className="flex-auto bg-stone-50 px-4 pt-[58px]">
+				<div className="flex flex-col">
+					<div className="my-2 flex flex-none space-x-2 rounded-md border border-solid border-stone-300 p-3">
+						<figure className="flex-none">
+							<img
+								src=""
+								alt="avatar"
+								className="size-28 rounded-full bg-red-100"
+							/>
+						</figure>
+						<div className="flex flex-col p-2 text-sm">
+							<span>이메일</span>
+							<span>위치</span>
+							<span></span>
+							<span>평점</span>
+							<span>수정하기</span>
+							<span>로그아웃</span>
+						</div>
+					</div>
+
+					<div className="flex flex-auto flex-col">
+						<div className="flex justify-between rounded-md border border-solid border-stone-300 p-3">
+							<div>
+								<button>등록한 책</button>
+								<button>찜한 책</button>
+							</div>
+							<div>
+								<span>옵션</span>
+							</div>
+						</div>
+						<div>
+							<ul>
+								탭 목록
+								<li>
+									이ㅏㄴ러미ㅏㅇ너라ㅣㅁ넝라ㅣ먼어리ㅗ마넝로ㅓㅏㅁ농라ㅓ모
+								</li>
+								<li>
+									이ㅏㄴ러미ㅏㅇ너라ㅣㅁ넝라ㅣ먼어리ㅗ마넝로ㅓㅏㅁ농라ㅓ모
+								</li>
+								<li>
+									이ㅏㄴ러미ㅏㅇ너라ㅣㅁ넝라ㅣ먼어리ㅗ마넝로ㅓㅏㅁ농라ㅓ모
+								</li>
+								<li>
+									이ㅏㄴ러미ㅏㅇ너라ㅣㅁ넝라ㅣ먼어리ㅗ마넝로ㅓㅏㅁ농라ㅓ모
+								</li>
+								<li>
+									이ㅏㄴ러미ㅏㅇ너라ㅣㅁ넝라ㅣ먼어리ㅗ마넝로ㅓㅏㅁ농라ㅓ모
+								</li>
+								<li>
+									이ㅏㄴ러미ㅏㅇ너라ㅣㅁ넝라ㅣ먼어리ㅗ마넝로ㅓㅏㅁ농라ㅓ모
+								</li>
+								<li>
+									이ㅏㄴ러미ㅏㅇ너라ㅣㅁ넝라ㅣ먼어리ㅗ마넝로ㅓㅏㅁ농라ㅓ모
+								</li>
+								<li>
+									이ㅏㄴ러미ㅏㅇ너라ㅣㅁ넝라ㅣ먼어리ㅗ마넝로ㅓㅏㅁ농라ㅓ모
+								</li>
+								<li>
+									이ㅏㄴ러미ㅏㅇ너라ㅣㅁ넝라ㅣ먼어리ㅗ마넝로ㅓㅏㅁ농라ㅓ모
+								</li>
+								<li>
+									이ㅏㄴ러미ㅏㅇ너라ㅣㅁ넝라ㅣ먼어리ㅗ마넝로ㅓㅏㅁ농라ㅓ모
+								</li>
+								<li>
+									이ㅏㄴ러미ㅏㅇ너라ㅣㅁ넝라ㅣ먼어리ㅗ마넝로ㅓㅏㅁ농라ㅓ모
+								</li>
+								<li>
+									이ㅏㄴ러미ㅏㅇ너라ㅣㅁ넝라ㅣ먼어리ㅗ마넝로ㅓㅏㅁ농라ㅓ모
+								</li>
+								<li>
+									이ㅏㄴ러미ㅏㅇ너라ㅣㅁ넝라ㅣ먼어리ㅗ마넝로ㅓㅏㅁ농라ㅓ모
+								</li>
+								<li>
+									이ㅏㄴ러미ㅏㅇ너라ㅣㅁ넝라ㅣ먼어리ㅗ마넝로ㅓㅏㅁ농라ㅓ모
+								</li>
+								<li>
+									이ㅏㄴ러미ㅏㅇ너라ㅣㅁ넝라ㅣ먼어리ㅗ마넝로ㅓㅏㅁ농라ㅓ모
+								</li>
+								<li>
+									이ㅏㄴ러미ㅏㅇ너라ㅣㅁ넝라ㅣ먼어리ㅗ마넝로ㅓㅏㅁ농라ㅓ모
+								</li>
+								<li>
+									이ㅏㄴ러미ㅏㅇ너라ㅣㅁ넝라ㅣ먼어리ㅗ마넝로ㅓㅏㅁ농라ㅓ모
+								</li>
+								<li>
+									이ㅏㄴ러미ㅏㅇ너라ㅣㅁ넝라ㅣ먼어리ㅗ마넝로ㅓㅏㅁ농라ㅓ모
+								</li>
+								<li>
+									이ㅏㄴ러미ㅏㅇ너라ㅣㅁ넝라ㅣ먼어리ㅗ마넝로ㅓㅏㅁ농라ㅓ모
+								</li>
+								<li>
+									이ㅏㄴ러미ㅏㅇ너라ㅣㅁ넝라ㅣ먼어리ㅗ마넝로ㅓㅏㅁ농라ㅓ모
+								</li>
+								<li>
+									이ㅏㄴ러미ㅏㅇ너라ㅣㅁ넝라ㅣ먼어리ㅗ마넝로ㅓㅏㅁ농라ㅓ모
+								</li>
+								<li>
+									이ㅏㄴ러미ㅏㅇ너라ㅣㅁ넝라ㅣ먼어리ㅗ마넝로ㅓㅏㅁ농라ㅓ모
+								</li>
+								<li>
+									이ㅏㄴ러미ㅏㅇ너라ㅣㅁ넝라ㅣ먼어리ㅗ마넝로ㅓㅏㅁ농라ㅓ모
+								</li>
+								<li>
+									이ㅏㄴ러미ㅏㅇ너라ㅣㅁ넝라ㅣ먼어리ㅗ마넝로ㅓㅏㅁ농라ㅓ모
+								</li>
+								<li>
+									이ㅏㄴ러미ㅏㅇ너라ㅣㅁ넝라ㅣ먼어리ㅗ마넝로ㅓㅏㅁ농라ㅓ모
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</main>
 
 			<ScrollRestoration />
 		</div>
