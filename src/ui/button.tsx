98421fc44609ed7c466462ce578ad5e135d46940
Withlaw <car2559@naver.com>
2024-01-13 15:19:41 +0900

    button ui style 수정

--- src/ui/button.tsx
+++ src/ui/button.tsx
@@ -8,7 +8,10 @@ type Props = {
 
 const Button = ({ children, options, onClick }: Props) => {
 	return (
-		<button className="h-8 w-full bg-green-600" onClick={onClick} {...options}>
+		<button
+			className="h-8 w-full rounded-md border border-green-500 bg-green-700 text-center text-sm text-stone-50 outline-none hover:bg-green-700/90 focus:ring-2 focus:ring-green-900/70 focus:ring-offset-1"
+			onClick={onClick}
+			{...options}>
 			{children}
 		</button>
 	);
