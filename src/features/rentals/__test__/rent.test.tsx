e9277e18b1d8454048e4dedad413d52abb7b1a9d
Withlaw <car2559@naver.com>
2024-01-24 19:52:34 +0900

    rent 테스트 코드 추가

--- src/features/rentals/__test__/rent.test.tsx
+++ src/features/rentals/__test__/rent.test.tsx
@@ -1,4 +1,4 @@
-import { describe, expect } from 'vitest';
+import { describe, expect, test } from 'vitest';
 
 import { BookDataFromServer } from '@/features/books/books.model.ts';
 import Rent from '@/features/rentals/rent.component.tsx';
@@ -6,11 +6,19 @@ import Rent from '@/features/rentals/rent.component.tsx';
 import { render, screen } from './utils.tsx';
 
 describe('Rent 컴포넌트', () => {
-	it('대여하기 버튼을 렌더링 한다.', () => {
+	test('rentalId가 존재하지 않으면 대여하기 버튼을 렌더링 한다.', () => {
 		render(<Rent book={{} as BookDataFromServer} />);
 
 		expect(
 			screen.getByRole('button', { name: '대여하기' }),
 		).toBeInTheDocument();
+	});
+
+	test('rentalId가 존재하면 대여하기 버튼을 렌더링 하지 않는다.', () => {
+		render(<Rent book={{ rentalId: 1 } as BookDataFromServer} />);
+
+		expect(
+			screen.queryByRole('button', { name: '대여하기' }),
+		).not.toBeInTheDocument();
 	});
 });
--- src/features/rentals/__test__/return.test.tsx
+++ src/features/rentals/__test__/return.test.tsx
@@ -6,7 +6,7 @@ import Return from '@/features/rentals/return.component.tsx';
 import { render, screen } from './utils.tsx';
 
 describe('Return 컴포넌트', () => {
-	test('렌탈 id가 존재하지 않으면 반납하기 버튼을 렌더링 하지 않는다.', () => {
+	test('rentalId가 존재하지 않으면 반납하기 버튼을 렌더링 하지 않는다.', () => {
 		render(<Return book={{} as BookDataFromServer} />);
 
 		expect(
@@ -14,7 +14,7 @@ describe('Return 컴포넌트', () => {
 		).not.toBeInTheDocument();
 	});
 
-	test('렌탈 id가 존재하면 반납하기 버튼을 렌더링 한다.', () => {
+	test('rentalId가 존재하면 반납하기 버튼을 렌더링 한다.', () => {
 		render(<Return book={{ rentalId: 1 } as BookDataFromServer} />);
 
 		expect(
