c0c93704f7112146fd33f75a00cad0b0dad5a946
Withlaw <car2559@naver.com>
2024-02-24 17:54:31 +0900

    confirm 창 테두리 색상 변경

--- src/ui/confirm.tsx
+++ src/ui/confirm.tsx
@@ -19,11 +19,11 @@ const Confirm = () => {
 		<Modal>
 			<Modal.Window name="" blured={false}>
 				<div className="absolute top-10 flex w-full justify-center">
-					<div className=" flex w-72 flex-col items-center justify-center rounded-md border border-stone-500 bg-[#fff] shadow-inner drop-shadow-xl">
+					<div className=" flex w-72 flex-col items-center justify-center rounded-md border border-stone-300 bg-[#fff] shadow-inner drop-shadow-lg">
 						<div className="min-h-20 w-full p-4 text-xs">
 							<p>{message}</p>
 						</div>
-						<div className=" flex w-full justify-center divide-x divide-stone-400 border-t border-stone-400 text-sm">
+						<div className=" flex w-full justify-center divide-x divide-stone-400 border-t border-stone-300 text-sm">
 							<button
 								onClick={cancelHandler}
 								className="w-full rounded-bl-md p-1 text-center text-green-700 hover:bg-stone-100 hover:text-green-600">
